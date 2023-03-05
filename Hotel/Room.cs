36456ddb250919f4d486e91be73a5b532350435a
DESKTOP-A8CUS40\malyo <valentin.maliovanik@gmail.com>
2023-03-05 20:41:27 +0200

    Hotel1

---
+++ Hotel/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    internal class Room
+    {
+        public int IdRoom { get; set; }
+
+        public int NumberRoom { get; set; }
+
+        public bool Reservation { get; set; }
+        public DateTime StartDate = DateTime.Now;
+        public DateTime FinalDate = DateTime.Now;
+        public Client Client { get; set; }
+        public Room(int number, int idRoom, bool reservation)
+        {
+            IdRoom = idRoom;
+            NumberRoom = number;
+            Reservation = true; Reservation = reservation;
+
+        }
+        
+    }
+}
